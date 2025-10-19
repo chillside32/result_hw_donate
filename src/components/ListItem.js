fc9596eb9c905c4ad96fb890380b4056b3c04ed2
chillsde <GeraDenis@mail.ru>
2025-10-19 13:28:45 +0300

    proverka

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,9 @@
-import { Component } from '../core/Component';
+import { Component } from "../core/Component";
 
 export class ListItem extends Component {
   setup(props) {
-    this.$rootElement = document.createElement('div');
-    this.$rootElement.className = 'donate-item';
-
+    this.$rootElement = document.createElement("div");
+    this.$rootElement.className = "donate-item";
     // ...
   }
 }
